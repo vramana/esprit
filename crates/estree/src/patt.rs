1d239803217bd69b052a7eae3c3182cf54c1be85
Ramana Venkata <vramana@users.noreply.github.com>
2018-09-07 02:04:33 +0530

    Parse ObjectPattern in estree

--- crates/estree/src/patt.rs
+++ crates/estree/src/patt.rs
@@ -1,9 +1,10 @@
-use easter::patt::{Patt, RestPatt, CompoundPatt};
+use easter::patt::{Patt, PropPatt, RestPatt, CompoundPatt};
 use easter::id::{Id, IdExt};
 use unjson::ty::Object;
 use unjson::{ExtractField, Unjson};
 
 use id::IntoId;
+use obj::IntoObj;
 use result::{Result, Map};
 use error::Error;
 use tag::{Tag, TagOf};
@@ -54,6 +55,37 @@ impl IntoPatt for Object {
                 let rest = rest.map(Box::new);
 
                 Ok(Patt::Compound(CompoundPatt::Arr(None, patt_elements, rest)))
+            }
+            Tag::ObjectPattern => {
+                let list = self.extract_array("properties")?;
+                let mut objs = list.map(|v| Ok(v.into_object().map_err(Error::Json)?))?;
+
+                // let mut rest = None;
+                // if let Some(mut last) = objs.pop() {
+                //     if last.tag()? == Tag::RestElement {
+                //         rest = Some(RestPatt {
+                //             location: None,
+                //             patt: last.extract_patt("argument")?
+                //         });
+                //     } else {
+                //         objs.push(last));
+                //     }
+                // }
+
+                let patt_elements = objs.map(|mut e| {
+                    match e.extract_bool("shorthand")? {
+                        true => Ok(PropPatt::Shorthand(e.extract_id("key")?)),
+                        false => {
+                            let prop_key = e.extract_object("key")?.into_prop_key()?;
+                            let prop_value = e.extract_object("value")?.into_patt()?;
+                            Ok(PropPatt::Regular(None, prop_key, prop_value))
+                        }
+                    }
+                })?;
+
+                // let rest = rest.map(Box::new);
+
+                Ok(Patt::Compound(CompoundPatt::Obj(None, patt_elements)))
             }
             _ => self.into_id().map(|id| id.into_patt())
         }
